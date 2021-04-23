c393eb16bbd2a93edd0417fb11333081b57dc8a3
Nate Sauber <nsauber@gmail.com>
2021-04-23 14:47:51 -0400

    Update nav list and simplify styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,38 @@ import './App.css';
 import { Link, Switch, Route } from "react-router-dom";
 
 const Home = () => ( <h2>Home</h2> );
-const Category = () => ( <h2>Category</h2> );
-const Products = () => ( <h2>Products</h2> );
+const Missions = () => ( <h2>Missions</h2> );
+const Rumors = () => ( <h2>Rumors</h2> );
+const Items = () => ( <h2>Items</h2> );
+const Clan = () => ( <h2>Clan</h2> );
 
 export default function App() {
   return (
     <>
 
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-1">
         <Link className="navbar-brand" to="/">FFTA Tactician</Link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon" />
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <Link className="nav-link" to="/category">Category</Link>
-            </li>
-            <li class="nav-item">
-              <Link className="nav-link" to="/products">Products</Link>
-            </li>
-          </ul>
+          <div class="navbar-nav">
+            <Link className="nav-link" to="/missions">Missions</Link>
+            <Link className="nav-link" to="/rumors">Rumors</Link>
+            <Link className="nav-link" to="/items">Items</Link>
+            <Link className="nav-link" to="/clan">Clan</Link>
+          </div>
         </div>
       </nav>
 
       <div className="container-fluid">
         <Switch>
           <Route exact path="/"><Home /></Route>
-          <Route path="/category"><Category /></Route>
-          <Route path="/products"><Products /></Route>
+          <Route path="/missions"><Missions /></Route>
+          <Route path="/rumors"><Rumors /></Route>
+          <Route path="/items"><Items /></Route>
+          <Route path="/clan"><Clan /></Route>
           <Route render={() => <h1>404: page not found</h1>} />
         </Switch>
       </div>
